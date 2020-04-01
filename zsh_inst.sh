42d23cfbf0157773201a51d4c6d466143f2fefad
SodiumFoo <3788126+happyda@users.noreply.github.com>
2020-04-02 07:52:50 +0800

    Add files via upload

---
+++ zsh_inst.sh
@@ -0,0 +1,10 @@
+/bin/sh
+
+sudo apt-get update ; sudo apt-get -y install zsh curl
+sudo mkdir -p /usr/local/fonts && cd /usr/local/fonts && curl --silent -fLo "Source Code Pro Regular Nerd Font Complete.ttf" https://github.com/ryanoasis/nerd-fonts/raw/master/patched-fonts/SourceCodePro/Regular/complete/Sauce%20Code%20Pro%20Nerd%20Font%20Complete.ttf
+
+cd ~ &&sudo fc-cache -fv
+sh -c "$(curl -fsSL https://raw.github.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
+git clone https://github.com/bhilburn/powerlevel9k.git ~/.oh-my-zsh/custom/themes/powerlevel9k
+git clone https://github.com/zsh-users/zsh-autosuggestions.git ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-autosuggestions
+git clone https://github.com/zsh-users/zsh-syntax-highlighting.git ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-syntax-highlighting
